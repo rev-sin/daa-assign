41a642c71a7f4ecb9110d93a85a77e7135f1e912
Revanth Singothu <revanthsingothu@gmail.com>
2024-08-22 11:42:51 +0530

    removed driver codes

--- q1.py
+++ q1.py
@@ -15,17 +15,17 @@ def partition(arr, low, high):
   
 	# Choose the pivot
 	pivot = arr[high]
-
+	
 	i = low - 1
-
+	
 	# Traverse arr[low..high] and move all smaller
-	# elements on the left side. Elements from low to
+	# elements on the left side. Elements from low to 
 	# i are smaller after every iteration
 	for j in range(low, high):
 		if arr[j] < pivot:
 			i += 1
-			arr[i], arr[j] = arr[j], arr[i]
-
+			arr[i], arr[j] = arr[i], arr[j]
+	
 	# Move pivot after smaller elements and
 	# return its position
 	arr[i + 1], arr[high] = arr[high], arr[i + 1]
@@ -42,24 +42,6 @@ def quick_sort(arr, low, high):
 		quick_sort(arr, low, pi - 1)
 		quick_sort(arr, pi + 1, high)
 
-# Function to print an array
-def print_array(arr):
-	for i in arr:
-		print(i, end=" ")
-	print()
-
-# Driver code
-if __name__ == "__main__":
-	arr = [10, 7, 8, 9, 1, 5]
-	print("Given array is")
-	print_array(arr)
-
-	quick_sort(arr, 0, len(arr) - 1)
-
-	print("\nSorted array is")
-	print_array(arr)
-	print("\ndone\n")
-
 """
 3-WAY MERGE SORT - https://www.geeksforgeeks.org/3-way-merge-sort/
 """
@@ -68,7 +50,6 @@ if __name__ == "__main__":
 and [mid2, high) mid1 is first midpoint 
 index in overall range to merge mid2 is second 
 midpoint index in overall range to merge"""
-
 
 def merge(gArray, low, mid1, mid2, high, destArray):
 	i = low
@@ -151,12 +132,10 @@ def merge(gArray, low, mid1, mid2, high, destArray):
 		l += 1
 		k += 1
 
-
 """ Performing the merge sort algorithm on the 
-given array of values in the rangeof indices 
+given array of values in the range of indices 
 [low, high). low is minimum index, high is 
 maximum index (exclusive) """
-
 
 def mergeSort3WayRec(gArray, low, high, destArray):
 	# If array size is 1 then do nothing
@@ -174,7 +153,6 @@ def mergeSort3WayRec(gArray, low, high, destArray):
 
 	# Merging the sorted arrays
 	merge(destArray, low, mid1, mid2, high, gArray)
-
 
 def mergeSort3Way(gArray, n):
 	# if array size is zero return null
@@ -198,24 +176,12 @@ def mergeSort3Way(gArray, n):
 	# return the sorted array
 	return gArray
 
-
-data = [45, -2, -45, 78, 30, -42, 10, 19, 73, 93]
-data = mergeSort3Way(data, 10)
-print("After 3 way merge sort: ", end="")
-for i in range(10):
-	print(f"{data[i]} ", end="")
-
-# This code is contributed by Susobhan Akhuli
-
-print("\ndone\n")
-
 """
 HEAP SORT - https://www.geeksforgeeks.org/heap-sort/
 """
 
 # To heapify subtree rooted at index i.
 # n is size of heap
-
 
 def heapify(arr, N, i):
 	largest = i  # Initialize largest as root
@@ -253,20 +219,6 @@ def heapSort(arr):
 		arr[i], arr[0] = arr[0], arr[i]  # swap
 		heapify(arr, i, 0)
 
-
-# Driver's code
-if __name__ == '__main__':
-	arr = [12, 11, 13, 5, 6, 7]
-
-	# Function call
-	heapSort(arr)
-	N = len(arr)
-
-	print("Sorted array is")
-	for i in range(N):
-		print("%d" % arr[i], end=" ")
-# This code is contributed by Mohit Kumra
-
 """
 BUCKET SORT - https://www.geeksforgeeks.org/bucket-sort-2/
 """
@@ -299,11 +251,6 @@ def bucket_sort(arr):
 		for num in bucket:
 			arr[index] = num
 			index += 1
-
-arr = [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434]
-bucket_sort(arr)
-print("Sorted array is:")
-print(" ".join(map(str, arr)))
 
 """
 RADIX SORT - https://www.geeksforgeeks.org/radix-sort/
@@ -339,76 +286,57 @@ class LinkedList:
 			current = current.next
 		return arr
 
-	# Function to create a linked list from a list
-	@classmethod
-	def from_list(cls, arr):
-		linked_list = cls()
-		for item in arr:
-			linked_list.append(item)
-		return linked_list
+	# Function to convert list to linked list
+	def from_list(self, arr):
+		self.head = None
+		for data in arr:
+			self.append(data)
 
-# A function to do counting sort of arr[] according to
-# the digit represented by exp.
-def countingSort(arr, exp1):
+	# Function to get the maximum value in the linked list
+	def get_max(self):
+		if not self.head:
+			return None
+		max_val = self.head.data
+		current = self.head.next
+		while current:
+			if current.data > max_val:
+				max_val = current.data
+			current = current.next
+		return max_val
 
-	n = len(arr)
+# Function to perform counting sort on the linked list based on digit represented by exp
+def counting_sort_linked_list(linked_list, exp):
+	output = LinkedList()
+	count = [0] * 10
 
-	# The output array elements that will have sorted arr
-	output = [0] * (n)
+	# Calculate count of occurrences
+	current = linked_list.head
+	while current:
+		index = (current.data // exp) % 10
+		count[index] += 1
+		current = current.next
 
-	# initialize count array as 0
-	count = [0] * (10)
-
-	# Store count of occurrences in count[]
-	for i in range(0, n):
-		index = arr[i] // exp1
-		count[index % 10] += 1
-
-	# Change count[i] so that count[i] now contains actual
-	# position of this digit in output array
+	# Update count to get positions
 	for i in range(1, 10):
 		count[i] += count[i - 1]
 
-	# Build the output array
-	i = n - 1
-	while i >= 0:
-		index = arr[i] // exp1
-		output[count[index % 10] - 1] = arr[i]
-		count[index % 10] -= 1
-		i -= 1
+	# Build output linked list
+	current = linked_list.head
+	output_list = [None] * len(linked_list.to_list())
+	while current:
+		index = (current.data // exp) % 10
+		output_list[count[index] - 1] = current.data
+		count[index] -= 1
+		current = current.next
 
-	# Copying the output array to arr[],
-	# so that arr now contains sorted numbers
-	for i in range(0, len(arr)):
-		arr[i] = output[i]
+	output.from_list(output_list)
+	return output
 
-# Method to do Radix Sort
-def radixSort(arr):
-
-	# Find the maximum number to know number of digits
-	max1 = max(arr)
-
-	# Do counting sort for every digit. Note that instead
-	# of passing digit number, exp is passed. exp is 10^i
-	# where i is current digit number
+# Radix sort function for linked list
+def radix_sort_linked_list(linked_list):
+	max_val = linked_list.get_max()
 	exp = 1
-	while max1 / exp >= 1:
-		countingSort(arr, exp)
+	while max_val // exp > 0:
+		linked_list = counting_sort_linked_list(linked_list, exp)
 		exp *= 10
-
-# Driver code
-arr = [170, 45, 75, 90, 802, 24, 2, 66]
-
-# Create a linked list from the array
-linked_list = LinkedList.from_list(arr)
-
-# Function Call on the linked list
-radixSort(linked_list.to_list())
-
-# Print sorted linked list
-sorted_arr = linked_list.to_list()
-for i in sorted_arr:
-	print(i, end=" ")
-
-# This code is contributed by Mohit Kumra
-# Edited by Patrick Gallagher
+	return linked_list
