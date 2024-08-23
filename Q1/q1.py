7be71470748ecb5a3207c86f9cc233c7268d0ea6
Revanth Singothu <revanthsingothu@gmail.com>
2024-08-23 23:11:23 +0530

    added changed to q1

--- Q1/q1.py
+++ Q1/q1.py
@@ -7,344 +7,285 @@ Bucket Sort - https://www.geeksforgeeks.org/bucket-sort-in-python/
 Radix Sort - https://www.geeksforgeeks.org/radix-sort/
 """
 
-"""
-QUICK SORT - https://www.geeksforgeeks.org/quick-sort-algorithm/
-"""
+class QuickSort:
+    def __init__(self):
+        self.swaps = 0
+        self.comparisons = 0
 
-def partition(arr, low, high):
-  
-	# Choose the pivot
-	pivot = arr[high]
-	
-	i = low - 1
-	
-	# Traverse arr[low..high] and move all smaller
-	# elements on the left side. Elements from low to 
-	# i are smaller after every iteration
-	for j in range(low, high):
-		if arr[j] < pivot:
-			i += 1
-			arr[i], arr[j] = arr[i], arr[j]
-	
-	# Move pivot after smaller elements and
-	# return its position
-	arr[i + 1], arr[high] = arr[high], arr[i + 1]
-	return i + 1
+    def partition(self, arr, low, high):
+        pivot = arr[high]
+        i = low - 1
 
-# The QuickSort function implementation
-def quick_sort(arr, low, high):
-	if low < high:
-		# pi is the partition return index of pivot
-		pi = partition(arr, low, high)
+        for j in range(low, high):
+            self.comparisons += 1
+            if arr[j] < pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+                self.swaps += 1
 
-		# Recursion calls for smaller elements
-		# and greater or equals elements
-		quick_sort(arr, low, pi - 1)
-		quick_sort(arr, pi + 1, high)
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        self.swaps += 1
+        return i + 1
 
-"""
-3-WAY MERGE SORT - https://www.geeksforgeeks.org/3-way-merge-sort/
-"""
+    def quick_sort(self, arr, low, high):
+        if low < high:
+            pi = self.partition(arr, low, high)
+            self.quick_sort(arr, low, pi - 1)
+            self.quick_sort(arr, pi + 1, high)
 
-""" Merge the sorted ranges [low, mid1), [mid1,mid2) 
-and [mid2, high) mid1 is first midpoint 
-index in overall range to merge mid2 is second 
-midpoint index in overall range to merge"""
+    def sort(self, arr):
+        self.quick_sort(arr, 0, len(arr) - 1)
+        return arr, self.swaps, self.comparisons
 
-def merge(gArray, low, mid1, mid2, high, destArray):
-	i = low
-	j = mid1
-	k = mid2
-	l = low
+class ThreeWayMergeSort:
+    def __init__(self):
+        self.swaps = 0
+        self.comparisons = 0
 
-	# Choose smaller of the smallest in the three ranges
-	while ((i < mid1) and (j < mid2) and (k < high)):
-		if(gArray[i] < gArray[j]):
-			if(gArray[i] < gArray[k]):
-				destArray[l] = gArray[i]
-				l += 1
-				i += 1
-			else:
-				destArray[l] = gArray[k]
-				l += 1
-				k += 1
-		else:
-			if(gArray[j] < gArray[k]):
-				destArray[l] = gArray[j]
-				l += 1
-				j += 1
-			else:
-				destArray[l] = gArray[k]
-				l += 1
-				k += 1
+    def merge(self, gArray, low, mid1, mid2, high, destArray):
+        i, j, k, l = low, mid1, mid2, low
 
-	# Case where first and second ranges
-	# have remaining values
-	while ((i < mid1) and (j < mid2)):
-		if(gArray[i] < gArray[j]):
-			destArray[l] = gArray[i]
-			l += 1
-			i += 1
-		else:
-			destArray[l] = gArray[j]
-			l += 1
-			j += 1
+        while i < mid1 and j < mid2 and k < high:
+            self.comparisons += 2
+            if gArray[i] < gArray[j]:
+                if gArray[i] < gArray[k]:
+                    destArray[l] = gArray[i]
+                    i += 1
+                else:
+                    destArray[l] = gArray[k]
+                    k += 1
+            else:
+                if gArray[j] < gArray[k]:
+                    destArray[l] = gArray[j]
+                    j += 1
+                else:
+                    destArray[l] = gArray[k]
+                    k += 1
+            l += 1
 
-	# case where second and third ranges
-	# have remaining values
-	while ((j < mid2) and (k < high)):
-		if(gArray[j] < gArray[k]):
-			destArray[l] = gArray[j]
-			l += 1
-			j += 1
-		else:
-			destArray[l] = gArray[k]
-			l += 1
-			k += 1
+        while i < mid1 and j < mid2:
+            self.comparisons += 1
+            if gArray[i] < gArray[j]:
+                destArray[l] = gArray[i]
+                i += 1
+            else:
+                destArray[l] = gArray[j]
+                j += 1
+            l += 1
 
-	# Case where first and third ranges have
-	# remaining values
-	while ((i < mid1) and (k < high)):
-		if(gArray[i] < gArray[k]):
-			destArray[l] = gArray[i]
-			l += 1
-			i += 1
-		else:
-			destArray[l] = gArray[k]
-			l += 1
-			k += 1
+        while j < mid2 and k < high:
+            self.comparisons += 1
+            if gArray[j] < gArray[k]:
+                destArray[l] = gArray[j]
+                j += 1
+            else:
+                destArray[l] = gArray[k]
+                k += 1
+            l += 1
 
-	# Copy remaining values from the first range
-	while (i < mid1):
-		destArray[l] = gArray[i]
-		l += 1
-		i += 1
+        while i < mid1 and k < high:
+            self.comparisons += 1
+            if gArray[i] < gArray[k]:
+                destArray[l] = gArray[i]
+                i += 1
+            else:
+                destArray[l] = gArray[k]
+                k += 1
+            l += 1
 
-	# Copy remaining values from the second range
-	while (j < mid2):
-		destArray[l] = gArray[j]
-		l += 1
-		j += 1
+        while i < mid1:
+            destArray[l] = gArray[i]
+            i += 1
+            l += 1
 
-	# Copy remaining values from the third range
-	while (k < high):
-		destArray[l] = gArray[k]
-		l += 1
-		k += 1
+        while j < mid2:
+            destArray[l] = gArray[j]
+            j += 1
+            l += 1
 
-""" Performing the merge sort algorithm on the 
-given array of values in the range of indices 
-[low, high). low is minimum index, high is 
-maximum index (exclusive) """
+        while k < high:
+            destArray[l] = gArray[k]
+            k += 1
+            l += 1
 
-def mergeSort3WayRec(gArray, low, high, destArray):
-	# If array size is 1 then do nothing
-	if (high - low < 2):
-		return
+    def mergeSort3WayRec(self, gArray, low, high, destArray):
+        if high - low < 2:
+            return
 
-	# Splitting array into 3 parts
-	mid1 = low + ((high - low) // 3)
-	mid2 = low + 2 * ((high - low) // 3) + 1
+        mid1 = low + ((high - low) // 3)
+        mid2 = low + 2 * ((high - low) // 3) + 1
 
-	# Sorting 3 arrays recursively
-	mergeSort3WayRec(destArray, low, mid1, gArray)
-	mergeSort3WayRec(destArray, mid1, mid2, gArray)
-	mergeSort3WayRec(destArray, mid2, high, gArray)
+        self.mergeSort3WayRec(destArray, low, mid1, gArray)
+        self.mergeSort3WayRec(destArray, mid1, mid2, gArray)
+        self.mergeSort3WayRec(destArray, mid2, high, gArray)
 
-	# Merging the sorted arrays
-	merge(destArray, low, mid1, mid2, high, gArray)
+        self.merge(destArray, low, mid1, mid2, high, gArray)
 
-def mergeSort3Way(gArray, n):
-	# if array size is zero return null
-	if (n == 0):
-		return
+    def sort(self, gArray):
+        n = len(gArray)
+        if n == 0:
+            return gArray, self.swaps, self.comparisons
 
-	# creating duplicate of given array
-	fArray = []
+        fArray = gArray.copy()
+        self.mergeSort3WayRec(fArray, 0, n, gArray)
+        gArray = fArray.copy()
+        return gArray, self.swaps, self.comparisons
 
-	# copying elements of given array into
-	# duplicate array
-	fArray = gArray.copy()
+class HeapSort:
+    def __init__(self):
+        self.swaps = 0
+        self.comparisons = 0
 
-	# sort function
-	mergeSort3WayRec(fArray, 0, n, gArray)
+    def heapify(self, arr, N, i):
+        largest = i
+        l = 2 * i + 1
+        r = 2 * i + 2
 
-	# copy back elements of duplicate array
-	# to given array
-	gArray = fArray.copy()
+        self.comparisons += 1
+        if l < N and arr[largest] < arr[l]:
+            largest = l
 
-	# return the sorted array
-	return gArray
+        self.comparisons += 1
+        if r < N and arr[largest] < arr[r]:
+            largest = r
 
-"""
-HEAP SORT - https://www.geeksforgeeks.org/heap-sort/
-"""
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            self.swaps += 1
+            self.heapify(arr, N, largest)
 
-# To heapify subtree rooted at index i.
-# n is size of heap
+    def sort(self, arr):
+        N = len(arr)
+        for i in range(N//2 - 1, -1, -1):
+            self.heapify(arr, N, i)
 
-def heapify(arr, N, i):
-	largest = i  # Initialize largest as root
-	l = 2 * i + 1     # left = 2*i + 1
-	r = 2 * i + 2     # right = 2*i + 2
+        for i in range(N-1, 0, -1):
+            arr[i], arr[0] = arr[0], arr[i]
+            self.swaps += 1
+            self.heapify(arr, i, 0)
 
-	# See if left child of root exists and is
-	# greater than root
-	if l < N and arr[largest] < arr[l]:
-		largest = l
+        return arr, self.swaps, self.comparisons
 
-	# See if right child of root exists and is
-	# greater than root
-	if r < N and arr[largest] < arr[r]:
-		largest = r
+class BucketSort:
+    def __init__(self):
+        self.swaps = 0
+        self.comparisons = 0
 
-	# Change root, if needed
-	if largest != i:
-		arr[i], arr[largest] = arr[largest], arr[i]  # swap
+    def insertion_sort(self, bucket):
+        for i in range(1, len(bucket)):
+            key = bucket[i]
+            j = i - 1
+            while j >= 0 and bucket[j] > key:
+                self.comparisons += 1
+                bucket[j + 1] = bucket[j]
+                j -= 1
+                self.swaps += 1
+            bucket[j + 1] = key
 
-		# Heapify the root.
-		heapify(arr, N, largest)
+    def sort(self, arr):
+        n = len(arr)
+        if n <= 1:
+            return arr, self.swaps, self.comparisons
 
-# The main function to sort an array of given size
+        max_val = max(arr)
+        buckets = [[] for _ in range(n)]
 
-def heapSort(arr):
-	N = len(arr)
+        for num in arr:
+            bi = min(int(n * num / (max_val + 1)), n - 1)
+            buckets[bi].append(num)
 
-	# Build a maxheap.
-	for i in range(N//2 - 1, -1, -1):
-		heapify(arr, N, i)
+        for bucket in buckets:
+            self.insertion_sort(bucket)
 
-	# One by one extract elements
-	for i in range(N-1, 0, -1):
-		arr[i], arr[0] = arr[0], arr[i]  # swap
-		heapify(arr, i, 0)
+        index = 0
+        for bucket in buckets:
+            for num in bucket:
+                arr[index] = num
+                index += 1
 
-"""
-BUCKET SORT - https://www.geeksforgeeks.org/bucket-sort-2/
-"""
+        return arr, self.swaps, self.comparisons
 
-def insertion_sort(bucket):
-	for i in range(1, len(bucket)):
-		key = bucket[i]
-		j = i - 1
-		while j >= 0 and bucket[j] > key:
-			bucket[j + 1] = bucket[j]
-			j -= 1
-		bucket[j + 1] = key
+class RadixSortLinkedList:
+    def __init__(self):
+        self.swaps = 0
+        self.comparisons = 0
 
-def bucket_sort(arr):
-    n = len(arr)
-    if n <= 1:
-        return
+    class Node:
+        def __init__(self, data):
+            self.data = data
+            self.next = None
 
-    # Find the maximum value to determine the range of buckets
-    max_val = max(arr)
+    class LinkedList:
+        def __init__(self):
+            self.head = None
 
-    # Create empty buckets
-    buckets = [[] for _ in range(n)]
+        def append(self, data):
+            if self.head is None:
+                self.head = RadixSortLinkedList.Node(data)
+            else:
+                current = self.head
+                while current.next:
+                    current = current.next
+                current.next = RadixSortLinkedList.Node(data)
 
-    # Put array elements in different buckets
-    for num in arr:
-        # Use min(num, max_val) to ensure the index is within bounds
-        bi = min(int(n * num / (max_val + 1)), n - 1)
-        buckets[bi].append(num)
+        def to_list(self):
+            arr = []
+            current = self.head
+            while current:
+                arr.append(current.data)
+                current = current.next
+            return arr
 
-    # Sort individual buckets using insertion sort
-    for bucket in buckets:
-        insertion_sort(bucket)
+        def from_list(self, arr):
+            self.head = None
+            for data in arr:
+                self.append(data)
 
-    # Concatenate all buckets into arr[]
-    index = 0
-    for bucket in buckets:
-        for num in bucket:
-            arr[index] = num
-            index += 1
+        def get_max(self):
+            if not self.head:
+                return None
+            max_val = self.head.data
+            current = self.head.next
+            while current:
+                if current.data > max_val:
+                    max_val = current.data
+                current = current.next
+            return max_val
 
-"""
-RADIX SORT - https://www.geeksforgeeks.org/radix-sort/
-"""
+    def counting_sort_linked_list(self, linked_list, exp):
+        output = self.LinkedList()
+        count = [0] * 10
 
-# Node class to represent each element in the linked list
-class Node:
-	def __init__(self, data):
-		self.data = data
-		self.next = None
+        current = linked_list.head
+        while current:
+            index = (current.data // exp) % 10
+            count[index] += 1
+            current = current.next
 
-# Linked list class
-class LinkedList:
-	def __init__(self):
-		self.head = None
+        for i in range(1, 10):
+            count[i] += count[i - 1]
 
-	# Function to insert a new node at the end
-	def append(self, data):
-		if self.head is None:
-			self.head = Node(data)
-		else:
-			current = self.head
-			while current.next:
-				current = current.next
-			current.next = Node(data)
+        current = linked_list.head
+        output_list = [None] * len(linked_list.to_list())
+        while current:
+            index = (current.data // exp) % 10
+            output_list[count[index] - 1] = current.data
+            count[index] -= 1
+            current = current.next
 
-	# Function to convert linked list to list
-	def to_list(self):
-		arr = []
-		current = self.head
-		while current:
-			arr.append(current.data)
-			current = current.next
-		return arr
+        output.from_list(output_list)
+        return output
 
-	# Function to convert list to linked list
-	def from_list(self, arr):
-		self.head = None
-		for data in arr:
-			self.append(data)
+    def radix_sort_linked_list(self, linked_list):
+        max_val = linked_list.get_max()
+        exp = 1
+        while max_val // exp > 0:
+            linked_list = self.counting_sort_linked_list(linked_list, exp)
+            exp *= 10
+        return linked_list
 
-	# Function to get the maximum value in the linked list
-	def get_max(self):
-		if not self.head:
-			return None
-		max_val = self.head.data
-		current = self.head.next
-		while current:
-			if current.data > max_val:
-				max_val = current.data
-			current = current.next
-		return max_val
-
-# Function to perform counting sort on the linked list based on digit represented by exp
-def counting_sort_linked_list(linked_list, exp):
-	output = LinkedList()
-	count = [0] * 10
-
-	# Calculate count of occurrences
-	current = linked_list.head
-	while current:
-		index = (current.data // exp) % 10
-		count[index] += 1
-		current = current.next
-
-	# Update count to get positions
-	for i in range(1, 10):
-		count[i] += count[i - 1]
-
-	# Build output linked list
-	current = linked_list.head
-	output_list = [None] * len(linked_list.to_list())
-	while current:
-		index = (current.data // exp) % 10
-		output_list[count[index] - 1] = current.data
-		count[index] -= 1
-		current = current.next
-
-	output.from_list(output_list)
-	return output
-
-# Radix sort function for linked list
-def radix_sort_linked_list(linked_list):
-	max_val = linked_list.get_max()
-	exp = 1
-	while max_val // exp > 0:
-		linked_list = counting_sort_linked_list(linked_list, exp)
-		exp *= 10
-	return linked_list
+    def sort(self, arr):
+        linked_list = self.LinkedList()
+        linked_list.from_list(arr)
+        sorted_linked_list = self.radix_sort_linked_list(linked_list)
+        return sorted_linked_list.to_list(), self.swaps, self.comparisons
--- Q1/q1testbench.py
+++ Q1/q1testbench.py
@@ -3,18 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Import sorting algorithms
-from Q1.q1 import quick_sort, mergeSort3Way, heapSort, bucket_sort, radix_sort_linked_list, LinkedList
+# Import sorting algorithm classes
+from q1 import QuickSort, ThreeWayMergeSort, HeapSort, BucketSort, RadixSortLinkedList
 
 
 # Helper functions for measuring performance
-def measure_performance(sort_function, arr):
-    """Measures execution time of the sorting function."""
+def measure_performance(sort_class, arr):
+    """Measures execution time, swaps, and comparisons of the sorting class."""
+    sorter = sort_class()  # Instantiate the sorting class
     start_time = time.time()
-    sort_function(arr)
+    sorted_arr, swaps, comparisons = sorter.sort(arr.copy())
     end_time = time.time()
     execution_time = (end_time - start_time) * 1000  # Convert to milliseconds
-    return execution_time
+    return execution_time, swaps, comparisons
 
 
 def generate_array(size):
@@ -22,7 +23,7 @@ def generate_array(size):
     return np.random.randint(0, 10000, size).tolist()
 
 
-def test_sorting_algorithm(sort_function, sizes):
+def test_sorting_algorithm(sort_class, sizes):
     """Tests the sorting algorithm and measures performance."""
     times = []
     for size in sizes:
@@ -30,9 +31,9 @@ def test_sorting_algorithm(sort_function, sizes):
 
         # Measure performance
         try:
-            exec_time = measure_performance(sort_function, arr.copy())
+            exec_time, swaps, comparisons = measure_performance(sort_class, arr)
             times.append(exec_time)
-            print(f"Size: {size}, Time: {exec_time:.4f} ms")
+            print(f"Size: {size}, Time: {exec_time:.4f} ms, Swaps: {swaps}, Comparisons: {comparisons}")
         except Exception as e:
             print(f"Error sorting array of size {size}: {e}")
             times.append(None)  # Append None to indicate error
@@ -40,10 +41,10 @@ def test_sorting_algorithm(sort_function, sizes):
     return times
 
 
-def plot_performance(sizes, times_dict):
-    """Plots performance results using Seaborn."""
+def plot_performance(sizes, times_dict, filename="sorting_performance.png"):
+    """Plots performance results using Seaborn and saves the plot to a file."""
     plt.figure(figsize=(12, 8))
-    sns.set_theme(style="whitegrid")  # Use set_theme instead of set
+    sns.set_theme(style="whitegrid")
     for label, times in times_dict.items():
         plt.plot(sizes, times, marker='o', label=label)
 
@@ -52,7 +53,8 @@ def plot_performance(sizes, times_dict):
     plt.title('Sorting Algorithms Performance')
     plt.legend()
     plt.grid(True)
-    plt.show()
+    plt.savefig(filename)
+    print(f"Plot saved as {filename}")
 
 
 # Sizes to test
@@ -60,36 +62,34 @@ sizes = [100, 500, 1000]
 
 # Test Quick Sort
 print("Testing Quick Sort")
-quick_sort_times = test_sorting_algorithm(lambda arr: quick_sort(arr, 0, len(arr) - 1), sizes)
+quick_sort_times = test_sorting_algorithm(QuickSort, sizes)
 
 # Test 3-Way Merge Sort
 print("Testing 3-Way Merge Sort")
-merge_sort_times = test_sorting_algorithm(lambda arr: mergeSort3Way(arr, len(arr)), sizes)
+merge_sort_times = test_sorting_algorithm(ThreeWayMergeSort, sizes)
 
 # Test Heap Sort
 print("Testing Heap Sort")
-heap_sort_times = test_sorting_algorithm(heapSort, sizes)
+heap_sort_times = test_sorting_algorithm(HeapSort, sizes)
 
 # Test Bucket Sort
 print("Testing Bucket Sort")
-bucket_sort_times = test_sorting_algorithm(bucket_sort, sizes)
+bucket_sort_times = test_sorting_algorithm(BucketSort, sizes)
 
 # Test Radix Sort
 print("Testing Radix Sort")
 
-
 def radix_sort_test(arr):
-    """Wraps radix_sort_linked_list for testing."""
+    """Wraps RadixSortLinkedList class for testing."""
     if len(arr) > 25:
-        return []
-    linked_list = LinkedList()
+        return arr  # Avoid testing with large sizes
+    linked_list = RadixSortLinkedList.LinkedList()
     for num in arr:
         linked_list.append(num)
-    sorted_linked_list = radix_sort_linked_list(linked_list)
+    sorted_linked_list = RadixSortLinkedList().sort(linked_list)
     return sorted_linked_list.to_list()
 
-
-radix_sort_times = test_sorting_algorithm(lambda arr: radix_sort_test(arr), sizes)
+radix_sort_times = test_sorting_algorithm(lambda arr, sizes=sizes: radix_sort_test(arr), sizes=sizes)
 
 # Plot the performance results
 times_dict = {
